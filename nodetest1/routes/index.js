77b13f47ea3301a5ee29b6a3dc2f2b1d9253b0c3
Robert <20748700+robdoescode@users.noreply.github.com>
2021-03-21 19:29:57 -0500

    added read/write portions for mongodb

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -11,4 +11,48 @@ router.get('/helloworld', function(req, res, next) {
   res.render('helloworld', { title: 'Hello, world!'});
 });
 
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('usercollection');
+  collection.find({},{},function(e,docs){
+    res.render('userlist', {
+      "userlist" : docs
+    });
+  });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+  res.render('newuser', {title: 'Add a new user'});
+});
+
+/* POST to add user service */
+router.post('/adduser', function(req, res) {
+  // set internal DB variable
+  var db = req.db;
+
+  // get form values.  these rely on the "name" attribute
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+
+  // set the collection
+  var collection = db.get('usercollection');
+
+  // finally, submit to the DB
+  collection.insert({
+    "username" : userName,
+    "email" : userEmail
+  }, function (err, doc) {
+    if (err) {
+      // if the insert failed, return an error
+      res.send("There was an error inserting the username and email to the database.");
+    }
+    else {
+      // no error, forward to the success page
+      res.redirect("userlist");
+    }
+  });
+});
+
 module.exports = router;
